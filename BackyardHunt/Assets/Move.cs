5be45f840a81e9ee220fa34b70a587ac5c78c5c6
Kenneth <alcanamay@gmail.com>
2023-09-24 23:33:10 -0600

    Cambios en move

--- BackyardHunt/Assets/Move.cs
+++ BackyardHunt/Assets/Move.cs
@@ -7,20 +7,22 @@ public class Move : MonoBehaviour
     public float speed = 100.0f;
     public float rot;
     public float rotSpeed;
+    public GameObject capsule;
    
 
     void Update()
     {
         float transalatez = Input.GetAxis("Vertical") * speed;
         float transalatex = Input.GetAxis("Horizontal") * speed;
+       
         transalatex *= Time.deltaTime;
         transalatez *= Time.deltaTime;
-
-        rot = Input.GetAxis("See") * rotSpeed;
-        Debug.Log(Input.GetAxis("See"));
         
-        transform.Translate(transalatex, 0, transalatez);
-        transform.eulerAngles = new Vector3(0, rot, 0);
 
+        rot = Input.GetAxis("See") * rotSpeed * Time.deltaTime;
+        //Debug.Log(Input.GetAxis("See"));
+        
+        transform.Translate(transalatex,0, transalatez);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + rot, 0);
     }
 }
