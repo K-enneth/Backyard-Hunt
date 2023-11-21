9b463e16ecdca79c4327710bfaf30848a6607c49
Kenneth <alcanamay@gmail.com>
2023-11-21 14:08:33 -0600

    Maquina de estados medianamente funcional

--- BackyardHunt/Assets/Scripts/IA/BehaviourController.cs
+++ BackyardHunt/Assets/Scripts/IA/BehaviourController.cs
@@ -5,26 +5,23 @@ using UnityEngine;
 public class BehaviourController : MonoBehaviour
 {
     public List<SteeringBehaviours> behaviours = new List<SteeringBehaviours>();
-    public Rigidbody rigidBody;
-    public Vector3 velocity;
-    public Vector3 totalForce = Vector3.zero;
+    
+    private Vector3 _velocity;
+    private Vector3 _totalForce = Vector3.zero;
 
     void FixedUpdate()
     {
-        //inicializando en ceros
-        totalForce = Vector3.zero;
-        
-        //Sacar el vector de la fuerza
+        _totalForce = Vector3.zero;
+
         foreach ( SteeringBehaviours behaviour in behaviours)
         {
             behaviour.Position = transform.position;
-            behaviour.Velocity = velocity;
-            totalForce += behaviour.GetForce();
+            behaviour.Velocity = _velocity;
+            _totalForce += behaviour.GetForce();
         }
 
-        velocity += totalForce;
-        transform.position += velocity * Time.deltaTime;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -60, 60), Mathf.Clamp(transform.position.y, -3, 3), Mathf.Clamp(transform.position.z, -60,60));
-        
+        _velocity += _totalForce;
+        transform.position += _velocity * Time.deltaTime;
+
     }
 }
--- BackyardHunt/Assets/Scripts/IA/Controllers/EnemyController.cs
+++ BackyardHunt/Assets/Scripts/IA/Controllers/EnemyController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,38 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private BehaviourController _behaviourController;
+    private Wander _wander;
+    private Seek _seek;
     public GameObject target;
+
+    public enum State
+    {
+        Idle,
+        Follow
+    }
+    public State enemyState;
+
+    private void Start()
+    {
+        _behaviourController = GetComponent<BehaviourController>();
+        _wander = GetComponent<Wander>();
+        _seek = GetComponent<Seek>();
+    }
+
+    private void Update()
+    {
+        switch (enemyState)
+        {
+            case State.Idle:
+                _behaviourController.behaviours[0] = _wander;
+                _wander.enabled = true;
+                break;
+            case State.Follow:
+                _behaviourController.behaviours[0] = _seek;
+                _wander.enabled = false;
+                _seek.Target = target.transform.position;
+                break;
+        }
+        
+    }
 }
